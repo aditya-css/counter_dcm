1a3cd3c13589ecc1d77fd5e7f5491f38668692bd
Aditya Chavda <aditya.c@simformsolutions.com>
2024-05-20 14:50:25 +0530

    :ok_hand: DCM Suggestion Changes

---
+++ lib/values/app_strings.dart
@@ -0,0 +1,7 @@
+class AppStrings {
+  static const increment = 'Increment';
+  static const pushedThisManyTimes = 'You have pushed the button this many times:';
+  static const appTitle = 'DCM Demo';
+
+  const AppStrings._();
+}
